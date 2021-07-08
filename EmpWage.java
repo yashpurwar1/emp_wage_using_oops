b80fc1f89f9e743ac7b1c142076a800fbe993290
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-09 00:57:24 +0530

    Added: functionality to calculate daily wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,14 +1,18 @@
 package empWage;
 
 public class EmpWage {
+	static final int wagePerHour=20;
+	static final int fullDayHour=8;
 	public static void main(String[] args) {
-		System.out.println("Welcome to employee wage problem");
-		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-		System.out.println(empCheck);
-		if (empCheck == 0) {
+		int dailyWage;
+		int empCheck=(int) Math.floor(Math.random()*10)%2;
+		if (empCheck==0) {
 			System.out.println("Employee is absent");
-		} else {
+		}
+		else {
 			System.out.println("Employee is present");
+			dailyWage=wagePerHour*fullDayHour;
+			System.out.println("Daily wage= "+dailyWage);
 		}
 	}
 }
