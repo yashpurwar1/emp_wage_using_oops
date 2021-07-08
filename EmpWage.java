0e2d2443e76eca01b52e6f819e9c08b5bfde9fff
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-09 01:05:43 +0530

    Refactored: Code using switch case

--- EmpWage.java
+++ EmpWage.java
@@ -1,24 +1,27 @@
 package empWage;
 
 public class EmpWage {
-	static int wagePerHour=20;
-	static int partTimeHour=4;
-	static int fullDayHour=8;
+	static int wagePerHour = 20;
+	static int partTimeHour = 4;
+	static int fullDayHour = 8;
+
 	public static void main(String[] args) {
 		int dailyWage, partTimeWage;
-		int empCheck=(int) Math.floor(Math.random()*10)%3;
-		if (empCheck==0) {
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+		case 0:
 			System.out.println("Employee is absent");
-		}
-		else if (empCheck==1){
+			break;
+		case 1:
 			System.out.println("Employee is present");
-			dailyWage=wagePerHour*fullDayHour;
-			System.out.println("DAily wage= "+dailyWage);
-		}
-		else {
+			dailyWage = wagePerHour * fullDayHour;
+			System.out.println("DAily wage= " + dailyWage);
+			break;
+		case 2:
 			System.out.println("Employee parttime present");
-			partTimeWage=partTimeHour*wagePerHour;
-			System.out.println("Parttime wage = "+partTimeWage);
+			partTimeWage = partTimeHour * wagePerHour;
+			System.out.println("Parttime wage = " + partTimeWage);
+			break;
 		}
 	}
 }
