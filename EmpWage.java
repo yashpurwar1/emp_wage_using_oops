e3b60926b7b556527035043ddab3597cae80416f
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-09 01:01:01 +0530

    Added: Functionality for part time wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,18 +1,24 @@
 package empWage;
 
 public class EmpWage {
-	static final int wagePerHour=20;
-	static final int fullDayHour=8;
+	static int wagePerHour=20;
+	static int partTimeHour=4;
+	static int fullDayHour=8;
 	public static void main(String[] args) {
-		int dailyWage;
-		int empCheck=(int) Math.floor(Math.random()*10)%2;
+		int dailyWage, partTimeWage;
+		int empCheck=(int) Math.floor(Math.random()*10)%3;
 		if (empCheck==0) {
 			System.out.println("Employee is absent");
 		}
-		else {
+		else if (empCheck==1){
 			System.out.println("Employee is present");
 			dailyWage=wagePerHour*fullDayHour;
-			System.out.println("Daily wage= "+dailyWage);
+			System.out.println("DAily wage= "+dailyWage);
+		}
+		else {
+			System.out.println("Employee parttime present");
+			partTimeWage=partTimeHour*wagePerHour;
+			System.out.println("Parttime wage = "+partTimeWage);
 		}
 	}
 }
