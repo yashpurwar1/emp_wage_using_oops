e07cfef2e7f84b560e93460c1efff13483814b8c
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-09 00:52:49 +0530

    Added: Functionality to check employee presenty

--- EmpWage.java
+++ EmpWage.java
@@ -3,5 +3,12 @@ package empWage;
 public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage problem");
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		System.out.println(empCheck);
+		if (empCheck == 0) {
+			System.out.println("Employee is absent");
+		} else {
+			System.out.println("Employee is present");
+		}
 	}
 }
